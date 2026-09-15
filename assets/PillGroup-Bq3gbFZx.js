74d24873cef33293125f878c03eb2744220ef33f
patrasxd <patrasxd@users.noreply.github.com>
2026-09-15 20:53:11 +0000

    deploy: 56d0d67a780a6d44a43558a3ec326e6e4128b984

---
+++ assets/PillGroup-Bq3gbFZx.js
@@ -0,0 +1,1 @@
+import"./index-wU6ijtIb.js";import{P as o}from"./PillGroup-KLkZMuqM.js";const i=o;export{i as P};
--- assets/PillGroup-yQcvfGY5.js
+++
@@ -1,1 +0,0 @@
-import"./index-BIQwxTCl.js";import{P as o}from"./PillGroup-khn4CLZc.js";const i=o;export{i as P};
