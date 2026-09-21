0417023e8a8d69fa4cc5e9134b264e98f665c1d1
patrasxd <patrasxd@users.noreply.github.com>
2026-09-21 13:14:01 +0000

    deploy: bbfcb37fcb0915518680ba76a16e51cdd8cf5258

---
+++ assets/ModeSelect-C3KWNWb3.js
@@ -0,0 +1,1 @@
+import"./index-BHXGOXlz.js";import{M as e}from"./ModeSelect-NQAk44Eu.js";const r=e;export{r as M};
--- assets/ModeSelect-CW_xIeTw.js
+++
@@ -1,1 +0,0 @@
-import"./index-BN3RJHUC.js";import{M as e}from"./ModeSelect-CRuEJJGU.js";const r=e;export{r as M};
